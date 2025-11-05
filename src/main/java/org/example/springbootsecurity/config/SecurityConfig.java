e340ea919eceb0bee58ca86d401a4e5ee26022de
Nicole0509 <niashavril0509@gmail.com>
2025-11-05 08:30:14 +0200

    Security configurations

--- src/main/java/org/example/springbootsecurity/config/SecurityConfig.java
+++ src/main/java/org/example/springbootsecurity/config/SecurityConfig.java
@@ -1,4 +1,49 @@
 package org.example.springbootsecurity.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+
+    // Define users (for demo purpose)
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User
+                .withDefaultPasswordEncoder()
+                .username("user")
+                .password("1234")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User
+                .withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin123")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    // Define security rules
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/home", "/login").permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults())  // enables form login
+                .httpBasic(Customizer.withDefaults()); // enables basic auth
+        return http.build();
+    }
 }
+
