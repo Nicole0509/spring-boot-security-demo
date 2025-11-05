d9f1646f61b5ba31b1f4c501d734adf6cd7a3aae
Nicole0509 <niashavril0509@gmail.com>
2025-11-05 08:27:03 +0200

    Got the basic setup for the poject

---
+++ src/main/java/org/example/springbootsecurity/config/SecurityConfig.java
@@ -0,0 +1,4 @@
+package org.example.springbootsecurity.config;
+
+public class SecurityConfig {
+}
---
+++ src/main/java/org/example/springbootsecurity/controllers/HomeController.java
@@ -0,0 +1,4 @@
+package org.example.springbootsecurity.controllers;
+
+public class HomeController {
+}
