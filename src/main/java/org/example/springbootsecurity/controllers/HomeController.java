f9b06cbb1c8eab5d870df99a88dac830de678b4e
Nicole0509 <niashavril0509@gmail.com>
2025-11-05 08:31:13 +0200

    Controller logic

--- src/main/java/org/example/springbootsecurity/controllers/HomeController.java
+++ src/main/java/org/example/springbootsecurity/controllers/HomeController.java
@@ -1,4 +1,23 @@
 package org.example.springbootsecurity.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to the public page!";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Welcome USER!";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Welcome ADMIN!";
+    }
 }
