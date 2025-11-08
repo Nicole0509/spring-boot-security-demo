4b1ecf9d48bbca117537c02cb0b02ca49ea7af89
Nicole0509 <niashavril0509@gmail.com>
2025-11-08 09:02:17 +0200

    Managed to capture my enum in a simplified way

--- src/main/java/org/example/springbootsecurity/Services/GenderService.java
+++ src/main/java/org/example/springbootsecurity/Services/GenderService.java
@@ -2,7 +2,6 @@ package org.example.springbootsecurity.Services;
 
 import org.example.springbootsecurity.DTOs.GenderDTO;
 import org.example.springbootsecurity.Models.Gender;
-import org.example.springbootsecurity.Models.GenderEnum;
 import org.example.springbootsecurity.Repositories.GenderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,13 +14,9 @@ public class GenderService {
 
     public Gender saveGender(GenderDTO genderDTO) {
         Gender gender = new Gender();
-        gender.setName(genderDTO.getName());
 
-        if (genderDTO.getGender() == GenderEnum.MALE) {
-            gender.setGender(GenderEnum.MALE);
-        } else {
-            gender.setGender(GenderEnum.FEMALE);
-        }
+        gender.setName(genderDTO.getName());
+        gender.setGender(genderDTO.getGender());
 
         return genderRepo.save(gender);
     }
